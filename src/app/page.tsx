cb268a67ccb94db8265675eae7d19791a5bc973a
Jenifer Angelo <jenmangelo@gmail.com>
2025-10-25 19:24:57 -0300

    Refactor HomePage component to display a gallery of images using mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
 import Link from "next/link";
 
+const mockUrls = ["https://djwpdjtwh8.ufs.sh/f/ZL5oB65u062XTazdHuZDGwLKqZt9xf0lVBEc1AsdFPjTknhi", "https://djwpdjtwh8.ufs.sh/f/ZL5oB65u062XZMABczu062XvhcBzs9GmPR8SNUuxIWOJdTM4", "https://djwpdjtwh8.ufs.sh/f/ZL5oB65u062XoG2Nehwei4up5LKYChHcf9UIFBDdwVlXvSTg", "https://djwpdjtwh8.ufs.sh/f/ZL5oB65u062XTyQ848ZDGwLKqZt9xf0lVBEc1AsdFPjTknhi", "https://djwpdjtwh8.ufs.sh/f/ZL5oB65u062X3oUMtZozvib3xAC0rlgHD9y7G1MWRhBd8csZ"];
+
+const mockImages = mockUrls.map ((url, index) => ({
+  id: index+1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="">
+      <div className = "flex flex-wrap gap-4">{
+        [...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image"/>
+          </div>
+        ))
+      }</div>
       Hello (gallery in progress)
     </main>
   );
